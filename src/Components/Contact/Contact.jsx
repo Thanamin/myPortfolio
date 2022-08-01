81d07467ba0f329105d0a49d1f14e0036a2e2dfa
Thanamin Akkharananwinit <mr.kan0m@hotmail.com>
2022-08-02 00:09:41 +0700

    update contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,27 +3,44 @@ import "./Contact.css";
 export const Contact = () => {
   return (
     <div id="Contact">
-      <div>
-        <a>Contact</a>
-        <a>Bankok, Thailand</a>
-        <a className="Email" href="mailto: thanamin.akk@gmail.com" >thanamin.akk@gmail.com</a>
-        <a className="Github" href="https://github.com/Thanamin" target="_blank" rel="noreferrer">
-          <img
-            src="../../../public/GitHub-Mark/PNG/GitHub-Mark-32px.png"
-            alt=""
-          />
-          GitHub
-        </a>
-        <a className="LinkedIn" href="https://www.linkedin.com/in/thanamin-akkharananwinit-7150b2240/" target="_blank" rel="noreferrer">
-          <img
-            src="../../../public/LinkedIn-Icon/icons8-linkedin-circled-60.png"
-            alt=""
-          />
-          LinkedIn
-        </a>
+      <div className="Contact-banner">
+        <div className="Contact-head">
+          <a>Contact</a>
+        </div>
+        <div className="Contact-city">
+          <a>Bankok, Thailand</a>
+        </div>
+        <div className="Tel">
+          <a>Tel : 088-5706796</a>
+        </div>
+
+        <div className="Contact-logo">
+          <a
+            className="Github"
+            href="https://github.com/Thanamin"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="../../../public/GitHub-Mark/PNG/GitHub-Mark-64px.png"
+              alt=""
+            />
+            GitHub
+          </a>
+          <a
+            className="LinkedIn"
+            href="https://www.linkedin.com/in/thanamin-akkharananwinit-7150b2240/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="../../../public/LinkedIn-Icon/icons8-linkedin-circled-60.png"
+              alt=""
+            />
+            LinkedIn
+          </a>
+        </div>
       </div>
     </div>
   );
 };
-
-
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,22 +3,40 @@ import "./Projects.css";
 export const Project = () => {
     return (
         <div id="Project">
-            <div>Project</div>
             <div>
-                <a>Colmar</a>
-                <img src="" />
-            </div>
-            <div>
-                <a>Find My Hat</a>
-                <img src="" />
-            </div>
-            <div>
-                <a>Jammming</a>
-                <img src="" />
-            </div>
-            <div>
-                <a>Fit2Be</a>
-                <img src="" />
+                <div className="Project-head">
+                    <a>Project</a>
+                </div>
+
+                <div className="Project-box">
+                    <div className="Project-col1">
+                        <div className="box">
+                            <a>Colmar</a>
+                            <img src="../../public/Project/Jammming.jpg" />
+                        </div>
+                        <div className="box">
+                            <a>Find My Hat</a>
+                            <img src="../../public/Project/Jammming.jpg" />
+                        </div>
+                    </div>
+
+                    <div className="Project-col2">
+                        <div className="box">
+                            <a
+                                href="http://thanamin_jammming.surge.sh/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                Jammming
+                            </a>
+                            <img src="../../public/Project/Jammming.jpg" />
+                        </div>
+                        <div className="box">
+                            <a>Fit2Be</a>
+                            <img src="../../public/Project/Jammming.jpg" />
+                        </div>
+                    </div>
+                </div>
             </div>
             {/* <div>
                 <button className="Project-Button-SeeMore" onClick="">See more on git hub</button>
