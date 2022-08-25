d9a06b15fe66ed342fab9acc4170bbce96388026
Thanamin Akkharananwinit <mr.kan0m@hotmail.com>
2022-08-25 10:04:28 +0700

    update img project don't link

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -19,7 +19,7 @@ export const Project = () => {
               >
                 Colmar
               </a>
-              <a href="https://thanamin.github.io/colmar/" target="_blank">
+              <a>
                 <img src="./Project/ColmarV2.jpg" />
               </a>
             </div>
@@ -31,11 +31,7 @@ export const Project = () => {
               >
                 Find Your Hat
               </a>
-              <a
-                href="https://replit.com/@ThanaminAkkhara/Find-Your-Hat-20Thanamin-Minmin#index.js"
-                target="_blank"
-                rel="noreferrer"
-              >
+              <a>
                 <img src="./Project/FindYourHatV2.jpg" />
               </a>
             </div>
@@ -50,11 +46,7 @@ export const Project = () => {
               >
                 Jammming
               </a>
-              <a
-                href="http://thanamin_jammming.surge.sh/"
-                target="_blank"
-                rel="noreferrer"
-              >
+              <a>
                 <img src="./Project/JammmingV2.jpg" />
               </a>
             </div>
@@ -64,9 +56,7 @@ export const Project = () => {
               href="https://fit2be-project-thanamin.vercel.app/"
               target="_blank"
               rel="noreferrer">Fit2Be</a>
-              <a href="https://fit2be-project-thanamin.vercel.app/"
-                target="_blank"
-                rel="noreferrer"><img src="./Project/2befitV2.jpg" /></a>
+              <a><img src="./Project/2befitV2.jpg" /></a>
             </div>
           </div>
         </div>
