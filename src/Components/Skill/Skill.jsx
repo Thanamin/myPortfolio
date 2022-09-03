8537757676d6928a46f3cc8d9222672a60808925
Thanamin Akkharananwinit <mr.kan0m@hotmail.com>
2022-09-03 23:58:30 +0700

    update icon skill slide

--- src/Components/Skill/Skill.jsx
+++ src/Components/Skill/Skill.jsx
@@ -1,14 +1,43 @@
 import "./Skill.css";
-import "./SkillMobile.css"
+import "./SkillMobile.css";
 
 export const Skill = () => {
   return (
     <div id="Skill">
       <div className="icon-banner">
         <div className="icon-banner-text">
-        <a>Skill</a>
+          <a>Skill</a>
         </div>
-        <div className="icon-banner-icon">
+
+
+        <div id="body-skill">
+          <div className="wrapper">
+            <div className="marquee">
+              <div className="marquee__group">
+                <img src="./Skill-icon/css-icon.png" />
+                <img src="./Skill-icon/html-icon.png" />
+                <img src="./Skill-icon/icons8-javascript-100-2.png" />
+                <img src="./Skill-icon/github-icon.png" />
+                <img src="./Skill-icon/icons8-bootstrap-100-2.png" />
+                <img src="./Skill-icon/icons8-mongodb-100.png" />
+                <img src="./Skill-icon/icons8-nodejs-100-2.png" />
+                <img src="./Skill-icon/icons8-react-native-100-2.png" />
+              </div>
+
+              <div aria-hidden="true" className="marquee__group">
+                <img src="./Skill-icon/css-icon.png" />
+                <img src="./Skill-icon/html-icon.png" />
+                <img src="./Skill-icon/icons8-javascript-100-2.png" />
+                <img src="./Skill-icon/github-icon.png" />
+                <img src="./Skill-icon/icons8-bootstrap-100-2.png" />
+                <img src="./Skill-icon/icons8-mongodb-100.png" />
+                <img src="./Skill-icon/icons8-nodejs-100-2.png" />
+                <img src="./Skill-icon/icons8-react-native-100-2.png" />
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="icon-banner-icon">
           <div className="Javascript-icon">
             <img src="./Skill-icon/icons8-javascript-500.png" />
           </div>
@@ -32,7 +61,7 @@ export const Skill = () => {
           </div>
           <div className="Github-icon">
             <img src="./Skill-icon/icons8-github-500.png" />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
