15b7537bb01be4cdcc210656d8a4867bb6514e40
Thanamin Akkharananwinit <mr.kan0m@hotmail.com>
2022-09-04 19:59:20 +0700

    add overlay project-link

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,9 +14,10 @@ export const Navbar = () => {
         <div className="Navbar-blank"></div>
         <Link className="link" spy={true} smooth={true} duration={500} to="Hello"><a href="#Hello">Home</a></Link>
         <Link className="link" spy={true} smooth={true} duration={500} to="AboutMe"><a href="#AboutMe">About</a></Link>
+        <Link className="link" spy={true} smooth={true} duration={500} to="Education"><a href="#Education">Education</a></Link>
         <Link className="link" spy={true} smooth={true} duration={500} to="Experience"><a href="#Experience">Experience</a></Link>
-        <Link className="link" spy={true} smooth={true} duration={500} to="Project"><a href="#Project">Project</a></Link>
         <Link className="link" spy={true} smooth={true} duration={500} to="Skill"><a href="#Skill">Skill</a></Link>
+        <Link className="link" spy={true} smooth={true} duration={500} to="Project"><a href="#Project">Project</a></Link>
         <a id="link-resume"
           href="https://drive.google.com/file/d/1IDSb45MzvFeXy7jjw5cliUgnLrW6BXv8/view?usp=sharing"
           target="_blank"
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -9,57 +9,265 @@ export const Project = () => {
           <a>Project</a>
         </div>
 
-        <div className="Project-box">
-          <div className="Project-col1">
-            <div className="box">
-              <a
-                href="https://thanamin.github.io/Colmar-Project/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Colmar
-              </a>
-              <a>
-                <img src="./Project/ColmarV2.jpg" />
-              </a>
-            </div>
-            <div className="box">
-              <a
-                href="https://replit.com/@ThanaminAkkhara/Find-Your-Hat-20Thanamin-Minmin#index.js"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Find Your Hat
-              </a>
-              <a>
-                <img src="./Project/FindYourHatV2.jpg" />
-              </a>
+        <div className="project-content">
+          <div className="box">
+            <a className="topic"
+            // href="https://thanamin.github.io/Colmar-Project/"
+            // target="_blank"
+            // rel="noreferrer"
+            >
+              Colmar
+            </a>
+            <div className="box-content">
+              <img
+                src="./Project/ColmarV2.jpg"
+                alt=""
+                className="project-img"
+              />
+              {/* Overlay */}
+              <div className="project-overlay">
+                {/* icon */}
+                <div className="blog-tags">
+                  <ul>
+                    <li>
+                      <a
+                        href="https://thanamin.github.io/Colmar-Project/"
+                        target="_blank"
+                      >
+                        <img
+                          src="./Internet-Icon/icons8-internet-100.png"
+                          className="box-icon-github"
+                        ></img>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://github.com/Thanamin/Colmar-Project"
+                        target="_blank"
+                      >
+                        <img
+                          src="./Github-Mark/PNG/icons8-github-100.png"
+                          className="box-icon-web"
+                        ></img>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                {/* Stack */}
+                {/* <p>Stack</p>
+                <div className="stack-icon">
+                  <ul>
+                    <li>
+                      <img src="./Skill-icon/css-icon.png" title="CSS" />
+                    </li>
+                    <li>
+                      <img src="./Skill-icon/html-icon.png" title="HTML" />
+                    </li>
+                  </ul>
+                </div> */}
+              </div>
             </div>
           </div>
 
-          <div className="Project-col2">
-            <div className="box">
-              <a
-                href="http://thanamin_jammming.surge.sh/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Jammming
-              </a>
-              <a>
-                <img src="./Project/JammmingV2.jpg" />
-              </a>
+          <div className="box">
+            <a className="topic"
+            // href="https://replit.com/@ThanaminAkkhara/Find-Your-Hat-20Thanamin-Minmin#index.js "
+            // target="_blank"
+            // rel="noreferrer"
+            >
+              Find Your Hat
+            </a>
+            <div className="box-content">
+              <img
+                src="./Project/FindYourHatV2.jpg"
+                alt=""
+                className="project-img"
+              />
+              {/* Overlay */}
+              <div className="project-overlay">
+                {/* icon */}
+                <div className="blog-tags">
+                  <ul>
+                    <li>
+                      <a
+                        href="https://replit.com/@ThanaminAkkhara/Find-Your-Hat-20Thanamin-Minmin#index.js"
+                        target="_blank"
+                      >
+                        <img
+                          src="./Internet-Icon/icons8-internet-100.png"
+                          className="box-icon-github"
+                        ></img>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://github.com/Thanamin/Find-Your-Hat"
+                        target="_blank"
+                      >
+                        <img
+                          src="./Github-Mark/PNG/icons8-github-100.png"
+                          className="box-icon-web"
+                        ></img>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                {/* Stack */}
+                {/* <p>Stack</p>
+                <div className="stack-icon">
+                  <ul>
+                    <li>
+                      <img src="./Skill-icon/js-icon.png" title="Javascript" />
+                    </li>
+                    <li>
+                      <img src="./Skill-icon/nodejs-icon.png" title="NodeJs" />
+                    </li>
+                  </ul>
+                </div> */}
+              </div>
             </div>
-            <div className="box">
-              <a
-              a
-              href="https://fit2be-project-thanamin.vercel.app/"
-              target="_blank"
-              rel="noreferrer">Fit2Be</a>
-              <a><img src="./Project/2befitV2.jpg" /></a>
+          </div>
+
+          <div className="box">
+            <a className="topic"
+            // href="http://thanamin_jammming.surge.sh/"
+            // target="_blank"
+            // rel="noreferrer"
+            >
+              Jammming
+            </a>
+            <div className="box-content">
+              <img
+                src="./Project/JammmingV2.jpg"
+                alt=""
+                className="project-img"
+              />
+              {/* Overlay */}
+              <div className="project-overlay">
+                {/* icon */}
+                <div className="blog-tags">
+                  <ul>
+                    <li>
+                      <a
+                        href="http://thanamin_jammming.surge.sh/"
+                        target="_blank"
+                      >
+                        <img
+                          src="./Internet-Icon/icons8-internet-100.png"
+                          className="box-icon-github"
+                        ></img>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://github.com/Thanamin/Jammming-Project"
+                        target="_blank"
+                      >
+                        <img
+                          src="./Github-Mark/PNG/icons8-github-100.png"
+                          className="box-icon-web"
+                        ></img>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                {/* Stack */}
+                {/* <p>Stack</p>
+                <div className="stack-icon">
+                  <ul>
+                    <li>
+                      <img src="./Skill-icon/css-icon.png" title="CSS" />
+                    </li>
+                    <li>
+                      <img src="./Skill-icon/html-icon.png" title="HTML" />
+                    </li>
+                    <li>
+                      <img src="./Skill-icon/js-icon.png" title="Javascript" />
+                    </li>
+                    <li>
+                      <img src="./Skill-icon/react-icon.png" title="React" />
+                    </li>
+                  </ul>
+                </div> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="box">
+            <a className="topic"
+            // href="https://fit2be-project-thanamin.vercel.app/"
+            // target="_blank"
+            // rel="noreferrer"
+            >
+              Fit2Be
+            </a>
+            <div className="box-content">
+              <img
+                src="./Project/2befitV2.jpg"
+                alt=""
+                className="project-img"
+              />
+              {/* Overlay */}
+              <div className="project-overlay">
+                {/* icon */}
+                <div className="blog-tags">
+                  <ul>
+                    <li>
+                      <a
+                        href="https://fit2be-project-thanamin.vercel.app/"
+                        target="_blank"
+                      >
+                        <img
+                          src="./Internet-Icon/icons8-internet-100.png"
+                          className="box-icon-github"
+                        ></img>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://github.com/Thanamin/fit2beProject"
+                        target="_blank"
+                      >
+                        <img
+                          src="./Github-Mark/PNG/icons8-github-100.png"
+                          className="box-icon-web"
+                        ></img>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                {/* Stack */}
+                {/* <p>Stack</p>
+                <div className="stack-icon">
+                  <ul>
+                    <li>
+                      <img src="./Skill-icon/css-icon.png" title="CSS" />
+                    </li>
+                    <li>
+                      <img src="./Skill-icon/html-icon.png" title="HTML" />
+                    </li>
+                    <li>
+                      <img src="./Skill-icon/js-icon.png" title="Javascript" />
+                    </li>
+                    <li>
+                      <img src="./Skill-icon/react-icon.png" title="React" />
+                    </li>
+                    <li>
+                      <img
+                        src="./Skill-icon/mongodb-icon.png"
+                        title="Mongodb"
+                      />
+                    </li>
+                    <li>
+                      <img src="./Skill-icon/nodejs-icon.png" title="NodeJs" />
+                    </li>
+                  </ul>
+                </div> */}
+              </div>
             </div>
           </div>
         </div>
+
       </div>
     </div>
   );
